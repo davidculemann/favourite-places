2bee650d728ef04243c2e378179bf7045bb4ca5c
David Culemann <davidculemann@gmail.com>
2021-10-25 16:24:38 +0400

    Replace PlaceList with PlaceListMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-import PlaceList from "./components/PlaceList";
+//import PlaceList from "./components/PlaceList";
+import PlaceListMap from "./components/PlaceListMap";
 
 function App(): JSX.Element {
   return (
     <>
       <Header />
-      <PlaceList />
+      <PlaceListMap />
     </>
   );
 }
